03835a6fce293e3f24c31fe14034a17647ecdc40
abraimi <abraimi@student.1337.ma>
2025-03-20 05:39:38 +0000

    Minor changes

--- ft_putnbr_printf.c
+++ ft_putnbr_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr_printf.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abraimi <abraimi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bl4ckr0s33 <bl4ckr0s33@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 07:47:35 by abraimi           #+#    #+#             */
-/*   Updated: 2025/03/13 02:57:05 by abraimi          ###   ########.fr       */
+/*   Updated: 2025/03/20 05:39:21 by bl4ckr0s33       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,9 @@ void	ft_putnbr_printf(long nbr, size_t *count)
 
 void	ft_putnbr_u_printf(unsigned int nbr, size_t *count)
 {
-	if (nbr >= 0 && nbr <= 9)
+	if (nbr == 0)
+		ft_putchar_printf(48, count);
+	else if (nbr > 0 && nbr <= 9)
 		ft_putchar_printf(nbr + 48, count);
 	else
 	{
